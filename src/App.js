ab05d6a12fe20f4c7132103b8cbbb3e07dd8308b
felipe-parra <fpr29.10@gmail.com>
2022-10-19 20:02:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main className="app">
+      <section className="container">
+        <article className="circle outer"></article>
+        <article className="circle inner"></article>
+      </section>
+    </main>
+  );
+}
